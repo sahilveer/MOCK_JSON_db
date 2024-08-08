b4aa5f43ed5dc5ed730ec58c49362899f42193f7
sahilveer <jassalgagandeep23@gmail.com>
2024-08-08 11:47:40 -0600

    My Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.set ('view engine','ejs');
+app.set ('views','./views');
+
+
+app.use(express.static('public'));
+
+
+
+app.listen(PORT,()=>{
+    console(`Connected on PORT ${PORT}`);
+
+});
